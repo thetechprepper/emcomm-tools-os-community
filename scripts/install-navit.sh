099be2a84e84207538fb85ef07bfdbb3dc25f54b
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2024-10-02 09:10:44 -0700

    Navit - Fixed car mode. Increased icon size for tablet

--- scripts/install-navit.sh
+++ scripts/install-navit.sh
@@ -8,10 +8,14 @@
 # The Navit package is broken in the Ubuntu repository. The following installation is based
 # on a fixed reported in https://github.com/navit-gps/navit/issues/1228
 
+# navit-gui-internal required for car display
+# navit-graphics-gtk-drawing-area required for desktop display
+
 apt install \
   navit \
   navit-gui-gtk \
   navit-graphics-gtk-drawing-area \
+  navit-gui-internal \
   libcanberra-gtk-dev \
   maptool \
   espeak \
