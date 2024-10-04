12d8d7cfe738225f67a4324c4f7f4e39932861d6
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2024-10-04 08:04:22 -0700

    Time - Added GPS as a time source

--- scripts/install-gps.sh
+++ scripts/install-gps.sh
@@ -2,11 +2,18 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 1 October 2022
-# Purpose : Configure GPS
+# Updated : 4 October 2024
+# Purpose : Configure GPS and time synch
 set -e
 
 et-log "Installing GPS packages and dependencies..."
-apt install gpsd gpsd-clients at -y
+apt install \
+  gpsd \
+  gpsd-clients \
+  gpsd-tools \
+  chrony \
+  at \
+  -y
 
 et-log "Installing gpsd config..."
 cp ../overlay/etc/default/gpsd /etc/default/
@@ -14,4 +21,11 @@ cp ../overlay/etc/default/gpsd /etc/default/
 et-log "Updating gpsd systemd script..."
 cp ../overlay/lib/systemd/system/gpsd.service /lib/systemd/system/
 systemctl daemon-reload
+
+et-log "Enabling gpsd..."
 systemctl enable gpsd
+
+et-log "Updating chronyd to support GPS as a time source..."
+echo "# ET: Enable GPS as a time source" >> /etc/chrony/chrony.conf
+echo "refclock SHM 0 refid GPS poll 4 precision 1e-3 offset 0.128" >> /etc/chrony/chrony.conf
+systemctl restart chronyd
