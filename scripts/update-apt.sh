39762a508008e1b9c6fe3f9ae3100f7fe076eb0e
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2024-10-06 08:48:53 -0700

    Attempt to prevent apt source list from getting overwritten during ISO image build

--- scripts/update-apt.sh
+++ scripts/update-apt.sh
@@ -2,9 +2,12 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
+# Updated : 6 October 2024
 # Purpose : Updates the apt repository
 
 et-log "Updating apt to use the old repository"
 
 cp -v ../overlay/etc/apt/sources.list /etc/apt/sources.list
+sudo chattr +i /etc/apt/sources.list
+
 apt update
