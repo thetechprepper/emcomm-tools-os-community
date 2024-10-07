cfef10f3628c025aa1172167dee6d7b1de6cafce
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2024-10-06 17:55:47 -0700

    Restored custom apt sources after image build

--- scripts/update-apt.sh
+++ scripts/update-apt.sh
@@ -3,11 +3,11 @@
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
 # Updated : 6 October 2024
-# Purpose : Updates the apt repository
+# Purpose : Updates the apt repository to use the old-releases 
 
-et-log "Updating apt to use the old repository"
+et-log "Updating apt to use the old repository..."
 
-cp -v ../overlay/etc/apt/sources.list /etc/apt/sources.list
-sudo chattr +i /etc/apt/sources.list
+cp -v ../overlay/etc/apt/sources.list /etc/apt/
+cp -v ../overlay/etc/crontab /etc/
 
 apt update
