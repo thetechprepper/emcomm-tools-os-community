69ef9270aef03d1595b296cb36ca623c71f5ee8c
Gaston Gonzalez <gaston.gonzalez@yahoo.com>
2024-10-09 14:40:49 -0700

    Created et-data group for shared data access; made et-data the default group for all user accounts; removed ET_USER as a service user is no longer required; set the default umask to 002; refactored Panasonic brightness fix to remove duplicative installation tasks; added first round of radio configuration

--- scripts/configure-user.sh
+++ scripts/configure-user.sh
@@ -2,14 +2,18 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
-# Updated : 1 October 2024
-# Purpose : Configure users
+# Updated : 9 October 2024
+# Purpose : Configure users and groups
 set -e
 
-et-log "Configuring users"
+et-log "Adding '${ET_GROUP}' group..."
+groupadd -g 1981  ${ET_GROUP} 
+
+et-log "Configuring users..."
 
 cp -r ../overlay/etc/skel /etc/
 [ ! -e /etc/skel/Desktop ] && mkdir /etc/skel/Desktop
 
-et-log "Add all users to dialout group"
+et-log "Add all users to dialout group..."
 cp -v -r ../overlay/etc/adduser.conf /etc/
+
