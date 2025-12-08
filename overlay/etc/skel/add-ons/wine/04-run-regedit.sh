a3b3ee295670dda1aa8c46a85b9db8f04b7b5f6d
thetechprepper <info@thetechprepper.com>
2025-12-08 15:46:13 -0700

    Post field testing updates - Automated addition of WINE registry keys. Applied fix for WINE application's (i.e. VARA) from constantly stealing the focus. in the desktop

--- overlay/etc/skel/add-ons/wine/04-run-regedit.sh
+++ overlay/etc/skel/add-ons/wine/04-run-regedit.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Author  : Gaston Gonzalez
 # Date    : 20 January 2025
-# Updated : 4 February 2025
+# Updated : 8 December 2025
 # Purpose : Run regedit
 set -e
 
 source ./common-checks.sh
 
+echo "Applying COM port mapping to /dev/et-cat..."
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\Ports' /t REG_SZ /v COM10 /d "/dev/et-cat" /f
+
+echo "Applying application foucs hijacking fix..."
+wine reg add 'HKEY_CURRENT_USER\Software\Wine\X11 Driver' /t REG_SZ /v UseTakeFocus /d N /f
+
 echo -e "${YELLOW}"
-echo -e "1. Expand HKEY_LOCAL_MACHINE > Software > Wine > Ports"
-echo -e "2. In the main pain, right-click and select 'New' > 'String Value'"
-echo -e "3. Set the name to ${WHITE}COM10${YELLOW} and the value to ${WHITE}/dev/et-cat${YELLOW}"
-echo -e "4. Close the Registry Editor application"
+echo -e "1. Expand HKEY_CURRENT_USER > Software > Wine > Ports"
+echo -e "   Confirm that ${WHITE}COM10${YELLOW} key present and is to ${WHITE}/dev/et-cat${YELLOW}"
+echo -e "2. Expand HKEY_CURRENT_USER > Software > Wine > X11 Driver"
+echo -e "   Confirm that ${WHITE}UseTakeFocus${YELLOW} key present and is to ${WHITE}N${YELLOW}"
+echo -e "3. Close the Registry Editor application"
 echo -e "${NC}"
 
 wine regedit
