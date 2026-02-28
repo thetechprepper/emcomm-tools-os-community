48c2ed8b4aa4e1bc37169295bce6a07a7488d7e9
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2026-02-28 22:11:06 +0100

    Install VarAC
    Have VarAC installed to the point where it can be started.

--- overlay/etc/skel/add-ons/wine/04-install-varac.sh
+++ overlay/etc/skel/add-ons/wine/04-install-varac.sh
@@ -2,6 +2,24 @@
 #
 # Purpose : Install VarAC
 
-# TODO
+winetricks \
+  --unattended \
+  vcrun2015 \
+  dotnet48 \
+  tahoma
+
+installer=$(find ${HOME}/Downloads -name VarAC_Installer_*.exe | sort -r | head -n1)
+
+if [ -z "${installer}" ]; then
+  echo -e "${YELLOW}VarAC can not be downloaded automatically"
+  echo -e "${WHITE}Visit https://www.varac-hamradio.com/download"
+  echo -e "From there, download the VarAC Windows Installer and place it into ~/Downloads"
+  echo -e "Rerun ./04-install-varac.sh${NC}"
+  exit 1
+fi
+
+if [ -f "${installer}" ]; then
+  wine "${installer}"
+fi
 
 echo -e "${YELLOW}Run ${WHITE}./05-run-regedit.sh${YELLOW} to run registry editor.${NC}"
