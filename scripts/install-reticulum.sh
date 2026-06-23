3f6017666838568eeea9192407f5e23d6e1fdbb1
thetechprepper <info@thetechprepper.com>
2026-06-23 10:52:43 -0700

    reticulum - Upgraded Reticulum to version 1.3.5

--- scripts/install-reticulum.sh
+++ scripts/install-reticulum.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 # Author  : Gaston Gonzalez
 # Date    : 13 February 2026
-# Updated : 15 June 2026
+# Updated : 23 June 2026
 # Purpose : Install Reticulum
 
-# Version pin Reticulum as we want to favor stability
-# above all other concerns (security, features, etc.).
-RNS_VERSION=1.2.5
-# WARNING: 1.3.5 does not work with MeshChat
-#RNS_VERSION=1.3.5
+RNS_VERSION=1.3.5
 
 et-log "Installing Python3..."
 apt install \
@@ -28,5 +24,5 @@ PYTHONUSERBASE=/etc/skel/.local \
   --no-index --find-links=wheelhouse \
   rns==${RNS_VERSION}
 
-et-log "Removing wheelhouse to save space..."
-[[ -e wheelhouse ]] && rm -vrf wheelhouse
+#et-log "Removing wheelhouse to save space..."
+#[[ -e wheelhouse ]] && rm -vrf wheelhouse
