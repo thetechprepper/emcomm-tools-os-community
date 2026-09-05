e922cfeab4a25bb4b9763ed3a02b2dcf63542fb8
thetechprepper <info@thetechprepper.com>
2026-09-05 14:51:24 -0700

    debranding - Replaced Plymouth spinners with EmComm Tools spinner

--- scripts/install-branding.sh
+++ scripts/install-branding.sh
@@ -2,6 +2,7 @@
 #
 # Author  : Gaston Gonzalez
 # Date    : 16 March 2024
+# Updated : 5 September 2026
 # Purpose : Install boot screen branding
 
 et-log "Installing EmComm Tools branding"
@@ -12,16 +13,19 @@ PLYMOUTH_DIR=/usr/share/plymouth
 BOOT_LOGO=/usr/share/plymouth/ubuntu-logo.png
 BOOT_LOGO_ORIG=$BOOT_LOGO.orig
 
-if [ ! -e $BOOT_LOGO_ORIG ]; then
+if [[ ! -e $BOOT_LOGO_ORIG ]]; then
   cp $BOOT_LOGO $BOOT_LOGO_ORIG
   et-log "Backing up boot screen logo: $BOOT_LOGO"
 fi
 
 et-log "Installing boot screen logo"
-cp ../logos/emcomm-tools-logo-white.png $BOOT_LOGO
+cp -v ../logos/emcomm-tools-logo-white.png $BOOT_LOGO
 
-et-log "Installing desktop wallpaper"
-cp ../logos/emcomm-tools-wallpaper.png /usr/share/backgrounds/
-cp ../logos/emcomm-tools-wallpaper.png /usr/share/backgrounds/warty-final-ubuntu.png
+et-log "Installing desktop wallpaper..."
+cp -v ../logos/emcomm-tools-wallpaper.png /usr/share/backgrounds/
+cp -v ../logos/emcomm-tools-wallpaper.png /usr/share/backgrounds/warty-final-ubuntu.png
+
+et-log "Installing spinners..."
+cp -v ../overlay/usr/share/plymouth/themes/spinner/*.png "${PLYMOUTH_DIR}/themes/spinner/"
 
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/backgrounds/emcomm-tools-wallpaper.png
